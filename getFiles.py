c247d50b56ed63aa8451df2bfc34a010adcbaf60
Franz Roth <franz.roth@roche.com>
2013-10-24 08:23:03 +0200

    initial commit

--- getFiles.py
+++ getFiles.py
@@ -8,6 +8,9 @@ import time
 import shutil
 import glob
 import tarfile
+import re
+
+
 
 def getOptions():
     currentDir = (os.path.dirname( os.path.realpath( __file__ ) ))
@@ -21,20 +24,31 @@ def getOptions():
     _add ("-o", "--output",         action="store",        dest="outputdir",     default=os.getenv("HOME") + "/Desktop", help="Target folder to copy the release to.")
     return parser
 
+
+class TargetFolderList():
+    (options, args)=getOptions().parse_args()
+    BuildLogs = "BuildLogs_InstrumentControl_" + options.icversion
+    UnitTestCoverage = "UnitTestCoverage_InstrumentControl_" + options.icversion
+    UnitTestResults = "UnitTestResults_InstrumentControl_" + options.icversion
+    Array = [BuildLogs,
+             UnitTestCoverage,
+             UnitTestResults]
+    
+
 def copyFiles(source, version):
     (options, args)=getOptions().parse_args()
     for file in glob.glob(source + '*_' + version + '_*.gz'):
-      if (("logs" in file) or ("rlx" in file) and ("artifacts" in file)): 
-        print "Copy " + file + " to " + options.outputdir        
+      if (("logs" in file) or ("rlx" in file) and ("artifacts" in file)):
+        print "Copy " + file + " to " + options.outputdir
         shutil.copy(file, options.outputdir)
 
 def copyAllFiles():
     (options, args)=getOptions().parse_args()
     print "Copy files from builddrop to " + options.outputdir
-    
-    source = "../../.gvfs//build_output on rkams889/Roche.DP.NewGen/IS/pkg_00000163_ISNewGen/"
-    copyFiles(source, options.icversion)
-    
+
+#    source = "../../.gvfs//build_output on rkams889/Roche.DP.NewGen/IS/pkg_00000163_ISNewGen/"
+#    copyFiles(source, options.icversion)
+
     source = "../../.gvfs//build_output on rkams889/Roche.DP.NewGen/IS/csm_00000136_SamplePrepProcessor/"
     copyFiles(source, options.sppversion)
 
@@ -43,26 +57,85 @@ def copyAllFiles():
 
     source = "../../.gvfs//build_output on rkams889/Roche.DP.NewGen/IS/csm_00000227_ADProcessor/"
     copyFiles(source, options.adpversion)
-    
+
 
 def unpackTAR():
-    print "Unpack the tar.gz"
     (options, args)=getOptions().parse_args()
-    for file in glob.glob(options.outputdir + "*"):
+    UnpackFolder = options.outputdir + "unpacked"
+    CreateFolderIfNotExists(UnpackFolder)
+    unpackArtifacts(options.outputdir, UnpackFolder)
+
+def unpackArtifacts(TarPath, UnpackFolder):
+    print "Unpack the artifacts.tar.gz"
+    FileList = glob.glob(TarPath + "*_artifacts.tar.gz")
+    for file in FileList:
         print "unpack " + file
-        TarFile = tarfile.open(file, 'r')
-        TarFile.extractall(file)
+        TarFile = tarfile.open(file)
+        TarFile.extractall(UnpackFolder)
+        TarFile.close()
+
+def copyLogArchived():
+    (options, args)=getOptions().parse_args()
+    LogsFolder = options.outputdir +  "BuildLogs_InstrumentControl_" + options.icversion
+    CreateFolderIfNotExists(LogsFolder)
+    FileList = glob.glob(options.outputdir + "*_logs.tar.gz")
+    for file in FileList:
+        print "copy " + file + " to " + LogsFolder
+        shutil.copy(file, LogsFolder)
+    
+def copyCovHtml():
+    (options, args)=getOptions().parse_args()
+    CovHTMLFolder = options.outputdir + "UnitTestCoverage_InstrumentControl_" + options.icversion
+    CreateFolderIfNotExists(CovHTMLFolder)
+    FolderList = os.listdir(options.outputdir + "unpacked/")
+    for folder in FolderList:
+        Source = options.outputdir + "unpacked/" + folder + "/CovHtml"
+        FolderSubSet = re.findall("[^_]+", folder)
+        Destination = CovHTMLFolder + "/" + FolderSubSet[2] + "_" + FolderSubSet[3] + "_UnitTestCoverageReport"
+        print "folder: " + folder + ", from Source: " + Source + ", To Destination: " + Destination
+        shutil.copytree(Source, Destination)
+    
+def copyTestResultPDF():
+    (options, args)=getOptions().parse_args()
+    LogsFolder = options.outputdir + "UnitTestResults_InstrumentControl_" + options.icversion
+    CreateFolderIfNotExists(LogsFolder)
+    FolderList = os.listdir(options.outputdir + "unpacked/")
+    for folder in FolderList:
+        Source = options.outputdir + "unpacked/" + folder + "/TestResults/Release/"
+        Destination = LogsFolder
+        print "copy *.pdf from " + Source + " to " + Destination
+        FileList = glob.glob(Source + "*.pdf")
+        for file in FileList:
+            print "copy " + file + " to " + Destination
+            shutil.copy(file, Destination)
+
+def deleteUnusedFilesAndFolders():
+    (options, args)=getOptions().parse_args()
+    deleteOutputFolder()
+    print "Delete unpacked folder from " + options.outputdir
+    shutil.rmtree(options.outputdir + "unpacked")
 
 def deleteOutputFolder():
     (options, args)=getOptions().parse_args()
     print "Delete *.gz files from " + options.outputdir
     for file in glob.glob(options.outputdir + "*.gz"):
-      os.remove(file)
+        os.remove(file)
+
+def CreateFolderIfNotExists(Folder):
+    if (os.path.exists(Folder)):
+        print "folder " + Folder + " does already exists. Delete before create new"
+        shutil.rmtree(Folder)
+    print "create folder " + Folder
+    os.makedirs(Folder)
 
 def main():
     deleteOutputFolder()
     copyAllFiles()
-#    unpackTAR()
+    unpackTAR()
+    copyLogArchived()
+    copyCovHtml()    
+    copyTestResultPDF()
+    deleteUnusedFilesAndFolders()
 
 if __name__ == '__main__':
     sys.exit(main())
